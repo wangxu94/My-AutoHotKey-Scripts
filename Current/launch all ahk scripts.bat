29c5f20424dcb7aa0296fd515c1dce81e763cc61
wangxu <wangxu94@gmail.com>
2016-09-26 21:48:27 +1300

    Batch files to launch and kill scripts

---
+++ Current/launch all ahk scripts.bat
@@ -0,0 +1,8 @@
+@echo off
+cd "C:\Users\xwang\Documents\My-AutoHotKey-Scripts\Current"
+start AutoCorrect.ahk
+start Brightness.ahk
+start Dictionary.ahk
+start MyHotKeys-v15.ahk
+start Thesaurus.ahk
+exit
